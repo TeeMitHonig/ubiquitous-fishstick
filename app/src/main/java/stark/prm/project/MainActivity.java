d7ec1c6c3e2a03b0d4d0d2952dee2d3099626af8
TeeMitHonig <164167101+TeeMitHonig@users.noreply.github.com>
2024-06-27 00:08:22 +0200

    Requesting permisson

--- app/src/main/java/stark/prm/project/MainActivity.java
+++ app/src/main/java/stark/prm/project/MainActivity.java
@@ -1,14 +1,21 @@
 package stark.prm.project;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_NOTIFICATION = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,26 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
             return insets;
         });
+
+        //Asking for permission to send notifications if their android version support it
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS},RC_NOTIFICATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //Test if Granted
+        if(requestCode == RC_NOTIFICATION ){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"ALLOWED",Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this,"DENIED",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
