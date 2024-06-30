bbd7ded6ef8ce6e669f4c8ebe4facee63cf9bc24
TeeMitHonig <164167101+TeeMitHonig@users.noreply.github.com>
2024-06-30 07:24:19 +0200

    Code Extrahiert und Debug Toasts entfernt

--- app/src/main/java/stark/prm/project/MainActivity.java
+++ app/src/main/java/stark/prm/project/MainActivity.java
@@ -10,9 +10,13 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import stark.prm.project.Notifications.NotificationHelper;
 
@@ -24,7 +28,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         notificationHelper = new NotificationHelper(this);
         notificationHelper.createNotificationChannel();
